c3cafdd3efe07c66dd47da95d205c8ba46350cb5
t.ito <t.ito@sonicmoov.com>
2021-06-10 02:53:36 +0900

    automate deploy

--- handler.js
+++ handler.js
@@ -5,7 +5,7 @@ module.exports.hello = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "こんにちは",
+        message: "Good",
         input: event,
       },
       null,
